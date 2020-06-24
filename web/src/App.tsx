a1dd43de111464836665789646aecadf422f5992
Davi Lopes <davi.lopes.amorim2014@hotmail.com>
2020-06-24 18:55:09 -0300

    basic

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+
+      <h1>conteudo da app</h1>
+    </div>
+
   );
 }
 
